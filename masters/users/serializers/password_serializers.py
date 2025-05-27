478d0963e047a14daaa4bebc2ab1410e06cd0757
Əbil Abilli <ebilebilli3@gmail.com>
2025-05-27 18:59:07 +0400

    Small change in for import ways

--- masters/users/serializers/password_serializers.py
+++ masters/users/serializers/password_serializers.py
@@ -1,9 +1,6 @@
-import redis
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
 from models.master_model import Master
-
-from masters.masters import settings
 from utils.otp import create_otp, send_otp, check_otp_in_redis, delete_otp_in_redis
 
 
@@ -35,7 +32,7 @@ class PasswordResetConfirmSerializer(serializers.Serializer):
 
         check_otp_in_redis(data)
         if data['new_password'] != data['new_password_two']:
-            raise serializers.ValidationError({'new_password2': 'Şifrələr uyğun deyil.'})
+            raise serializers.ValidationError({'new_password_two': 'Şifrələr uyğun deyil.'})
 
         user = Master.objects.get(phone_number=data['phone_number'])
         validate_password(data['new_password'], user=user)
