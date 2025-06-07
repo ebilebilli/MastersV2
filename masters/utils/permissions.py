7e59a2a5f0e5ee7a98cebedb394e48e4a1ca8543
Elvin <elvinhaxverdiyev777@gamil.com>
2025-06-07 09:58:49 +0400

    added dog strings for permisssion function

--- masters/utils/permissions.py
+++ masters/utils/permissions.py
@@ -3,6 +3,10 @@ from rest_framework import permissions
 
 
 class HeHasPermission(BasePermission):   
+    """
+    Custom permission to allow access only if the requesting user
+    is the owner of the object or is a staff member.
+    """
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user or request.user.is_staff
 
